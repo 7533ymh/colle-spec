53435c1fa6e2878e8c6cf1baafbe719ce7fdfb8c
7533ymh <ymh7532@icloud.com>
2021-04-13 23:57:40 +0900

    check vue

--- backend/src/main/java/com/example/backend/TestController.java
+++ backend/src/main/java/com/example/backend/TestController.java
@@ -1,6 +1,7 @@
 package com.example.backend;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/api")
 public class TestController {
 
-    @PostMapping("/ip")
-    public ResponseEntity<String> ip (HttpServletRequest request) {
-        // 요청을 보낸 클라이언트의 IP주소를 반환합니다.
-        return ResponseEntity.ok(request.getRemoteAddr());
+    @GetMapping("/test1")
+    public String getString() {
+        return "This is an example!!!";
     }
 }
