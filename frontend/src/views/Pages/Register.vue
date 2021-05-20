196fed5057c2c476c2f4ebfc856d18e1f97a3c61
DongminChoi <cdm0614@gmail.com>
2021-05-20 21:52:40 +0900

    회원가입 ui 수정 적용
    0520

--- frontend/src/views/Pages/Register.vue
+++ frontend/src/views/Pages/Register.vue
@@ -1,3 +1,6 @@
+
+<!-- 회원가입 vue -->
+
 <template>
   <div>
     <!-- Header -->
@@ -21,90 +24,97 @@
               <div class="text-center text-muted mb-4">
                 <h3>회원가입</h3>
               </div>
-              <!-- <validation-observer v-slot="{handleSubmit}" ref="formValidator"> -->
-                <b-form role="form">
+              <validation-observer v-slot="{handleSubmit}" ref="formValidator">
+                <b-form role="form" @submit.prevent="handleSubmit(btn_register)">
+                  
+                  
+              
 
+                    <div class="form-group mb-3">
+                    <input type="text" class="form-control" id="ID" v-model="user.id" placeholder="아이디" @blur="checkDuplicate" />
+                    <span class="badge badge-danger mt-1" v-if="!availableID">이미 사용중인 아이디입니다.</span>
+                    </div>
+                  
                   <base-input alternative
                               class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="아이디"
-                              :rules="{required: true}"
-                              v-model="user.id">
-                  </base-input>
-                  <button type="submit" @click="check">아이디중복체크</button>
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="이름(실명)"
-                              name="Name"
-                              :rules="{required: true}"
-                              v-model="user.name">
-                  </base-input>
-
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-email-83"
+                              
                               placeholder="이메일"
                               name="Email"
-                              :rules="{required: true, email: true}"
+                              
                               v-model="user.mail">
                   </base-input>
 
                   <base-input alternative
                               class="mb-3"
-                              prepend-icon="ni ni-lock-circle-open"
+                              
                               placeholder="비밀번호"
                               type="password"
                               name="Password"
-                              :rules="{required: true, min: 6}"
+                              
                               v-model="user.pass">
                   </base-input>
+
                   <base-input alternative
                               class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="학년(1~4)"
                               
-                              :rules="{required: true}"
-                              v-model="user.grade">
-                  </base-input>
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="성별(남,여)"
+                              placeholder="이름(실명)"
+                              name="Name"
                               
-                              :rules="{required: true}"
-                              v-model="user.sex">
+                              v-model="user.name">
                   </base-input>
+
                   <base-input alternative
                               class="mb-3"
-                              prepend-icon="ni ni-hat-3"
+                              
                               placeholder="휴대폰 번호"
+                              type="text"
+                              name="phone"
                               
-                              :rules="{required: true}"
                               v-model="user.phone">
                   </base-input>
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="희망직종"
-                              
-                              :rules="{required: true}"
-                              v-model="user.objective">
-                  </base-input>
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="희망기업"
-                              
-                              :rules="{required: true}"
-                              v-model="user.enterprise">
-                  </base-input>
+
                   
+                  
+                  <b-form-group label="성별" v-slot="{ ariaDescribedby }" v-model="user.sex">
+                    <b-form-radio  v-model="user.sex" :aria-describedby="ariaDescribedby" name="sex" value="male">남</b-form-radio>
+                    <b-form-radio  v-model="user.sex" :aria-describedby="ariaDescribedby" name="sex" value="female">여</b-form-radio>
+                  </b-form-group>
+                  
+
+                  <base-input alternative
+                              class="mb-3"
+                              
+                              placeholder="학년"
+                              type="number"
+                              name="grade"
+                              
+                              v-model="user.grade">
+                  </base-input>
+
+                  <base-input alternative
+                              class="mb-3"
+                              
+                              placeholder="목표기업"
+                              name="enterprise"
+                              
+                              v-model="user.enterprise ">
+                  </base-input>
+
+                  <base-input alternative
+                              class="mb-3"
+                              
+                              placeholder="목표직종"
+                              name="objective"
+                              
+                              v-model="user.objective ">
+                  </base-input>
+
+                 
                   <div class="text-center">
-                    <b-button type="submit" variant="primary" class="mt-4" @click="btn_register">가입하기</b-button>
+                    <b-button type="submit" variant="primary" class="mt-4">가입하기</b-button>
                   </div>
                 </b-form>
-              <!-- </validation-observer> -->
+              </validation-observer>
             </b-card-body>
           </b-card>
         </b-col>
@@ -121,12 +131,12 @@
       return {
         user: [{
        //유저정보
-        }]
+        }],
+        availableID:true
       }
     },
     methods: {
-      btn_register(event) {
-                event.preventDefault();
+      btn_register() {
                 
                 var url = "http://49.50.166.108:4000/api/signup";
 
@@ -147,7 +157,7 @@
                             alert(response.data.msg)
                             // 성공적으로 회원가입이 되었을 경우
                             this
-                                .$routes.routes
+                                .$router
                                 .push({name: 'Login1'}); //로그인화면으로 넘어간다.
                         }
                         console.log(response);
@@ -158,22 +168,21 @@
                         alert(error.response.data.msg) //회원가입 실패시 에러메시지
                     });
             },
-
-            //아이디 중복체크 함수
-            check(event) {
-                event.preventDefault();
-                var url = "http://49.50.166.108:4000/api/signup/check/";
-                axios
-                    .get(url + this.user.id)
-                    .then(response => {
-                        alert(response.data.msg)
-                        console.log(response);
-                    })
-                    .catch(err => {
-                        alert(err.response.data.msg)
-                        console.log(err.response.data.msg)
-                    });
-            },
+          async checkDuplicate() {
+            var url = "http://49.50.166.108:4000/api/signup/check/";
+                          await axios
+                              .get(url + this.user.id)
+                              .then(response => {
+                                  console.log(response);
+                                  this.availableID = true;
+                                console.log('avail:',this.availableID)
+                              })
+                              .catch(err => {
+                                  console.log(err.response.data.msg)
+                                  this.availableID = false;
+                                  console.log('avail:',this.availableID)
+                              });
+                      },
         }
     }
 </script>
