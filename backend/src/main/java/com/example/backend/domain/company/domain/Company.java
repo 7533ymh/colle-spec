05a2fc71abeb3d16563e3a1f5b27c9fdb78f3788
7533ymh <ymh7532@icloud.com>
2021-05-26 02:56:44 +0900

    " 회사 내용 구체화 "

--- backend/src/main/java/com/example/backend/domain/company/domain/Company.java
+++ backend/src/main/java/com/example/backend/domain/company/domain/Company.java
@@ -10,19 +10,19 @@ import lombok.*;
 public class Company {
 
     private int idx;
-    private String name;
-    private String industry;
-    private String people;
-    private String division;
-    private String establishment;
-    private String capital;
-    private String take;
-    private String representative;
-    private String salary;
-    private String bussiness;
-    private String insurance;
-    private String url;
-    private String address;
+    private String name; //회사 이름
+    private String industry; // 산업
+    private String people; // 사원 수
+    private String division; // 기업구분
+    private String establishment;// 설립일
+    private String capital; //자본금
+    private String take; //매출액
+    private String representative; //대표자
+    private String salary; // 대졸초임
+    private String bussiness; // 주요사업
+    private String insurance; // 4대 보험
+    private String url;  // 홈페이지
+    private String address; // 주소
     private int pass_award;
     private int pass_career;
     private int pass_certificate;
